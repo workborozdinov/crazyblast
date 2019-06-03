8848e1457a25bf8889703b322cf58d9a08cdbf7e
workborozdinov <workborozdinov@gmail.com>
2019-06-03 16:27:41 +0300

    sample project settings

---
+++ src/scenes/GameScene.js
@@ -0,0 +1,9 @@
+import {CST} from "../CST";
+
+export class GameScene extends Phaser.Scene{
+    constructor(){
+        super({
+            key: CST.SCENES.GAME
+        })
+    }
+}
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path')
+
+const conf = {
+
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: 'main.js',
+        publicPath: 'dist/'
+    },
+    devServer: {
+        overlay: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+            }
+        ]
+    },
+    devtool: 'eval-sourcemap'
+
+};
+
+module.exports = conf;
